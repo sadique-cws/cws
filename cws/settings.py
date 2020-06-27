4192711e218375454fe6267bdaba823195ac7f5a
sadique hussain <32757358+sadique-cws@users.noreply.github.com>
2020-06-27 16:17:35 +0530

    Update settings.py

--- cws/settings.py
+++ cws/settings.py
@@ -166,13 +166,3 @@ MESSAGE_TAGS = {
 LOGIN_REDIRECT_URL = 'login_success'
 
 LOGOUT_REDIRECT_URL = 'accounts/login/'
-
-PAYTM_COMPANY_NAME = "Code with SadiQ"   # For representation purposes
-PAYTM_INDUSTRY_TYPE_ID = "Retail"     # For staging environment
-PAYTM_CHANNEL_ID = "WEB"
-PAYTM_MERCHANT_KEY = "H1J3@E1#NsG!vsSy"
-PAYTM_MERCHANT_ID = "Orbmou57491502380880"
-PAYTM_CALLBACK_URL = "http://localhost:8000/student/response/" # Hardcode
-PAYTM_WEBSITE = "DEFAULT"
-PAYTM_PAYMENT_GATEWAY_URL = "https://securegw.paytm.in/order/process"
-PAYTM_TRANSACTION_STATUS_URL = "https://securegw.paytm.in/order/status"
